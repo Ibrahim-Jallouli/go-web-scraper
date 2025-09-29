0091c379c76680f19cbd0d397c6d6178b466b5f6
Ibrahim-Jallouli <ibrahim.jallouli@alumni.univ-avignon.fr>
2025-09-29 21:13:49 +0200

    ajout de la methode retrieveSiteMapURL

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+func retrieveSiteMapURL(websiteUrl string) string {
+	if !strings.HasSuffix(websiteUrl, "/") {
+		websiteUrl += "/"
+	}
+	websiteUrl += "robots.txt"
+
+	resp, err := http.Get(websiteUrl)
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println(err, " ", resp.Status)
+		return ""
+	}
+	scanner := bufio.NewScanner(resp.Body)
+	var sitemapURL string
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		fmt.Println((line))
+		if strings.HasPrefix(strings.ToLower(line), "sitemap:") {
+			parts := strings.SplitN(line, ":", 2)
+			if len(parts) == 2 {
+				sitemapURL = strings.TrimSpace(parts[1])
+				break
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Erreur lecture :", err)
+	}
+	return sitemapURL
+}
+
 func main() {
+
+	sitemapUrl := retrieveSiteMapURL("https://facebook.com/")
+	fmt.Println(sitemapUrl)
 	fmt.Println("Hello, Go!")
-	res, err := http.Get("https://www.google.com/")
+	res, err := http.Get("https://raidlight.com/sitemap.xml")
 
 	fmt.Println(res.Status)
 	fmt.Println(err)
